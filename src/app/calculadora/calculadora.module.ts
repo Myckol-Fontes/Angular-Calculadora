6fe034f06713d69d7856c5284abbb9ababfafe56
Myckol Fontes <myckol.ti@gmail.com>
2022-05-21 18:09:06 -0300

    Exibindo model Calculadora na tela

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalculadoraComponent } from './componentes';
+import { CalculadoraComponent } from './calculadora';
 
 
 
 @NgModule({
   declarations: [
+    CalculadoraComponent
+  ],
+  exports: [
     CalculadoraComponent
   ],
   imports: [
