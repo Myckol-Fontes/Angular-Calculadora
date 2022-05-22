f1132f481a7b985ff21cbb7f54b39fa6c2dfed76
Myckol Fontes <myckol.ti@gmail.com>
2022-05-22 14:22:56 -0300

    Teste unitário do serviço calculadora

--- src/app/calculadora/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora/calculadora.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CalculadoraService } from '../services';
+
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
 
-  ngOnInit(): void {
+  constructor(private calculadoraService: CalculadoraService) { }
+
+  ngOnInit() {
+  	this.limpar();
   }
+
+  /**
+   * Inicializa todos os operadores para os valores padrão.
+   *
+   * @return void
+   */
+  limpar(): void {
+  	this.numero1 = '0';
+  	this.numero2 = null;
+  	this.resultado = null;
+  	this.operacao = null;
+  }
+
+
 
 }
